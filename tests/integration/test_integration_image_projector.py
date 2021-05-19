669d64ef3d587c80b823e81e649b8ca4dbee3eaa
lordofbejgli <kanyur@gmail.com>
2021-05-19 14:19:08 +0200

    Add test cases for the updated projection parameter calculations.

--- tests/integration/test_integration_image_projector.py
+++ tests/integration/test_integration_image_projector.py
@@ -62,7 +62,64 @@ class TestOld(unittest.TestCase):
 
         self.assertTrue(np.allclose(points, points_ref))
 
+class TestNew(unittest.TestCase):
 
+    def compare_points(self, R0, theta0, z0, Rp, thetap, zp, view):
+        """
+        Helper function. Compares the projected coordinates 
+        of reference points with the expected values. 
+        """
+        points = get_reference_points('tests/integration/fixtures/%s.dat' % view)
+        points_ref = get_reference_points('tests/integration/fixtures/%s_ref.dat' % view)
+        view = ImageProjector(R0, theta0, z0, Rp, thetap, zp, old=False)
+
+        view.calculate_parameters(points_ref[0, 10], points_ref[1, 10],
+                                  points_ref[0, 14], points_ref[1, 14], 
+                                  points[:, 10:11], points[:, 14:15])
+
+        points = view.calc_pixel_coord(points)
+
+        return np.allclose(points, points_ref)
+
+    def test_aeq20(self):
+        view = 'aeq20'
+        R0 = 6.34811
+        z0 = -0.649178
+        theta0 = 1.21099
+        Rp = 6.21000
+        zp = 0.810000
+        thetap = -0.0400000
+        self.assertTrue(self.compare_points(R0, theta0, z0, Rp, thetap, zp, view))
+    
+    def test_aeq31(self):
+        view = 'aeq31'
+        R0 = 6.44669
+        z0 = 0.665888
+        theta0 = 2.54492
+        Rp = 7.44
+        zp = 0.48
+        thetap = 4.01
+        self.assertTrue(self.compare_points(R0, theta0, z0, Rp, thetap, zp, view))
+        
+    def test_aeq41(self):
+        view = 'aeq41'
+        R0 = 6.34720
+        z0 = 0.643984
+        theta0 = 3.81564
+        Rp = 6.31000
+        zp = -0.560000
+        thetap = -1.15000
+        self.assertTrue(self.compare_points(R0, theta0, z0, Rp, thetap, zp, view))
+
+    def test_aeq50(self):
+        view = 'aeq50'
+        R0 = 6.34459
+        z0 = -0.643974
+        theta0 = -1.30240
+        Rp = 6.31000
+        zp = 0.560000
+        thetap = 3.72000
+        self.assertTrue(self.compare_points(R0, theta0, z0, Rp, thetap, zp, view))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
