bb54874cb726a2fcb80b5a5ba91829161569f600
lordofbejgli <kanyur@gmail.com>
2021-06-23 15:02:34 +0200

    Add EOF NL.

--- errors.py
+++ errors.py
@@ -4,3 +4,4 @@ class NoSurfaceFileError(Exception):
     def __init__(self, number):
         self.message = f'No file found for surface number: {number}'
         super().__init__(self.message)
+        
