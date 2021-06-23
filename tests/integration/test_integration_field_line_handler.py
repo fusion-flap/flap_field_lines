2b200b1c8c9b26440faa8000a43922c511aee928
lordofbejgli <kanyur@gmail.com>
2021-06-23 14:24:47 +0200

    Add descriptions to tests.

--- tests/integration/test_integration_field_line_handler.py
+++ tests/integration/test_integration_field_line_handler.py
@@ -20,8 +20,52 @@ class TestConstructor(unittest.TestCase):
     their location.
     """
     def setUp(self) -> None:
-        #Write flux surface files location here
-        self.path = '/media/data/w7x_flux_surfaces'
-        self.config = 'EIM'
+        """
+        Test initiation. Initializes path for test data and a few parameters of 
+        the reading. Lines are chose with multiple selection, while the toroidal 
+        coordinate selection is given as a range.
+        """
+        #Give flux surface files location here
+        self.path = '/media/data/w7x_flux_surfaces/test'
+        self.lines = (5, 60, 120, 240)
+        self.tor_range = '0:500:50'
 
-    
+    def test_reading_all_lines(self):
+        """
+        Reading all lines from flux surface file.
+        """
+        field_lines = FieldLineHandler(self.path, surface=40)
+        self.assertEqual(field_lines.get_field_lines().shape, (3, 360, 3651))
+
+    def test_read_directions(self):
+        """
+        These tests check the reading of field lines in various directions.
+        """
+        field_lines = FieldLineHandler(self.path, surface=40, lines=self.lines, 
+                                       tor_range=self.tor_range, direction='backward')
+        self.assertEqual(field_lines.get_field_lines().shape, (3, 4, 10))
+        field_lines = FieldLineHandler(self.path, surface=40, lines=self.lines, 
+                                       tor_range=self.tor_range, direction='both')
+        self.assertEqual(field_lines.get_field_lines().shape, (3, 4, 10))
+        field_lines_2 = FieldLineHandler(self.path, surface=40, lines=self.lines, 
+                                       tor_range='-1:-500:-50', direction='backward')
+        self.assertTrue(np.array_equal(field_lines.get_field_lines(), 
+                                        field_lines_2.get_field_lines()))
+
+    def test_read_multiple_files(self):
+        """
+        Checks reading from multiple files. Also tests correct behaviour if 
+        some files are not found.
+        """
+        field_lines = FieldLineHandler(self.path, lines=self.lines, 
+                                       tor_range=self.tor_range)
+        self.assertEqual(field_lines.get_field_lines().shape, (3, 4, 10, 2))
+        field_lines = FieldLineHandler(self.path, surface=(25, 30, 35),
+                                       lines=self.lines, tor_range=self.tor_range)
+        self.assertEqual(field_lines.get_field_lines().shape, (3, 4, 10))
+        field_lines = FieldLineHandler(self.path, surface=[30, 35, 40],
+                                       lines=self.lines, tor_range=self.tor_range)
+        self.assertEqual(field_lines.get_field_lines().shape, (3, 4, 10, 2))
+
+        
+        
