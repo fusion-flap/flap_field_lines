a1fc4422f922cc60cb23d12a040071bfc95c3e2a
lordofbejgli <kanyur@gmail.com>
2021-12-16 11:27:22 +0100

    Add rounding to pixel_2_array.

--- accessories.py
+++ accessories.py
@@ -55,7 +55,7 @@ def plot_frame(data, frame, x=[316, 707], y=[384, 639]):
 def pixel_2_array(x, y, x0, y0):
     x = x - x0
     y = y-y0
-    return x, y
+    return np.rint(x), np.rint(y)
 
 def array_2_pixel(x, y, x0, y0):
     x = x + x0
