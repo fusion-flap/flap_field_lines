0fa6c80d5f701585b1292b37adb89382c857653c
lordofbejgli <kanyur@gmail.com>
2022-02-11 11:00:55 +0100

    Complete refactor of correlator.py.

--- correlator.py
+++ correlator.py
@@ -9,11 +9,7 @@ import imp
 import io
 import pickle
 import os
-from typing import Protocol
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-from scipy.integrate import trapz
 import flap_field_lines.field_line_handler as flh
 import flap_field_lines.image_projector as imp
 import flap_field_lines.accessories as acc
@@ -61,9 +57,9 @@ class Correlator:
             self.surface = None
             self.lines = None
 
-        self.pol_selection = []
-        self.tor_selection = []
-    
+        self.selection = []
+        self.selection_type = None
+
     def update_lines(self, surface, view, config='EIM'):
         if type(view) is not imp.ImageProjector:
             raise TypeError('View should be of type ImageProjector!íqn')
@@ -72,34 +68,105 @@ class Correlator:
         self.surface = surface
 
     def select_contour(self, tor_ind, pol_sel):
-        self.tor_selection = flh.process_selection(tor_sel)
-        self.tor_selection = [tor_ind for i in range(len(self.pol_selection))]
+        pol_sel = flh.process_selection(pol_sel)
+        self.selection = [[i, tor_ind] for i in pol_sel]
+        self.remove_out_of_frame()
+        self.selection_type = 'tor'
 
     def select_line(self, tor_sel, pol_ind):
-        self.tor_selection = flh.process_selection(tor_sel)
-        self.pol_selection = [pol_ind for i in range(len(self.tor_selection))]
+        tor_sel = flh.process_selection(tor_sel)
+        self.selection = [[pol_ind, i] for i in tor_sel]
+        self.remove_out_of_frame()
+        self.selection_type = 'pol'
 
     def select(self, tor_sel, pol_sel):
-        self.tor_selection = flh.process_selection(tor_sel)
-        self.tor_selection = flh.process_selection(tor_sel)
-        if len(self.pol_selection) != len(self.tor_selection):
+        tor_sel = flh.process_selection(tor_sel)
+        pol_sel = flh.process_selection(pol_sel)
+        if len(tor_sel) != len(pol_sel):
             raise ValueError('Numer of given poloidal coordinates does not \
                               match the number of toroidal coordinates.\n')
+        self.selection = [[pol_sel[i], tor_sel[i]] for i in range(len(tor_sel))]
+        self.remove_out_of_frame()
+        self.selection_type = 'mix'
 
-    def correlate(self):
-        for i in range(len(self.pol_selection)):
-            pol, tor = self.pol_selection[i], self.tor_selection[i]
-            xp, yp = acc.pixel_2_array(self.lines[0, pol, tor], 
-                                       self.lines[1, pol, tor], 
+    def remove_out_of_frame(self):
+        for sel in self.selection:
+            xp, yp = acc.pixel_2_array(self.lines[0, sel[0], sel[1]], 
+                                       self.lines[1, sel[0], sel[1]], 
                                        self.x[0], self.y[0], self.dx, self.dy)
             if (xp >= self.data.shape[0]) or (xp < 0) or (yp >= self.data.shape[1]) or (yp < 0):
-                continue        
-            d_ref = self.data.data[xp, yp, :]
-            d_ref = flap.DataObject(data_array = d_ref, 
-                                    data_unit = self.dunit, 
-                                    coordinates = [self.t], 
-                                    data_title = 'Reference', 
-                                    exp_id = '123456_01')
-            data_ccf = self.data.ccf(ref=d_ref, coordinate='Time', 
-                                     options={'Normalize' : True})
-            data_ccf.save(self.save_path + f'/ccf_{self.surface}_{tor}_{pol}.dat', protocol=4)
+                self.selection.remove(sel)
+
+    def return_corr(self, i):
+        pol, tor = self.selection[i][0], self.selection[i][1]
+        xp, yp = acc.pixel_2_array(self.lines[0, pol, tor], 
+                                   self.lines[1, pol, tor], 
+                                   self.x[0], self.y[0], self.dx, self.dy)
+                
+        d_ref = self.data.data[xp, yp, :]
+        d_ref = flap.DataObject(data_array = d_ref, 
+                                data_unit = self.dunit, 
+                                coordinates = [self.t], 
+                                data_title = 'Reference', 
+                                exp_id = '123456_01')
+        data_ccf = self.data.ccf(ref=d_ref, coordinate='Time', 
+                                 options={'Normalize' : True})
+        t = acc.return_coord(data_ccf, 'Time lag')
+        mid_p = int(np.floor(len(t)) / 2)
+        data_ccf = data_ccf.slice_data(slicing={'Time lag' : t[mid_p-15:mid_p+16]})
+        return data_ccf
+
+    def correlate(self):
+        data_ccf = self.return_corr(0)
+        
+        t = data_ccf.get_coordinate_object('Time lag')
+        x = data_ccf.get_coordinate_object('Image x')
+        y = data_ccf.get_coordinate_object('Image y')
+        dunit = data_ccf.data_unit
+
+        ccf_arrays = data_ccf.data
+        if len(self.selection) > 1:
+            ccf_arrays = ccf_arrays[..., np.newaxis]
+        else:
+            pass
+
+        for i in range(1, len(self.pol_selection)):
+            data_ccf = self.return_corr(i)
+            ccf_arrays = np.concatenate((ccf_arrays, 
+                                         data_ccf.data[..., np.newaxis]), 
+                                         axis = -1)
+        
+        ref_coord_id = ""
+
+        if self.selection_type == 'tor':
+            ref_coord = flap.Coordinate(name='Poloidal coordinate', 
+                                        mode=flap.CoordinateMode(equidistant=False), 
+                                        unit = 'deg', 
+                                        shape=(len(self.selection)), 
+                                        start=self.selection[0][0], 
+                                        values= [sel[0] for sel in self.selection], 
+                                        dimension_list=[3])
+            ref_coord_id = f'_{self.selection[0][1]}'
+        elif self.selection_type == 'pol':
+            ref_coord = flap.Coordinate(name='Toroidal coordinate', 
+                                        mode=flap.CoordinateMode(equidistant=False), 
+                                        unit = 'bin', 
+                                        shape=(len(self.selection)), 
+                                        start=self.selection[0][1],
+                                        values= [sel[1] for sel in self.selection], 
+                                        dimension_list=[3])
+            ref_coord_id = f'_{self.selection[0][0]}'
+        elif self.selection_type == 'mix':
+            ref_coord = flap.Coordinate(name='Reference point order', 
+                                        mode=flap.CoordinateMode(equidistant=True), 
+                                        unit = 'bin', 
+                                        shape=(len(self.selection)), 
+                                        start=0,
+                                        values= [i for i in range(len(self.selection))], 
+                                        dimension_list=[3])
+
+        data_ccf = flap.DataObject(data_array = ccf_arrays, 
+                                   data_unit = dunit, 
+                                   coordinates = [t, x, y, ref_coord])
+
+        data_ccf.save(self.save_path + f'/ccf_{self.surface}_{self.selection_type}{ref_coord_id}.dat', protocol=4)
