82c758153ad56664705b17393df1c7b4cccf991d
lordofbejgli <kanyur@gmail.com>
2022-02-18 11:47:28 +0100

    Add create_slides.

--- corr_plotter.py
+++ corr_plotter.py
@@ -37,6 +37,8 @@ class CorrPlotter:
         else:
             self.save_path = os.path.dirname(corr_file)
 
+        self.save_title = corr_file.split('.')[0]
+
         _, self.lines = acc.get_lines(self.data.info['surface'], 
                                       ':', 'both', 
                                       self.data.info['view'], 
@@ -48,58 +50,77 @@ class CorrPlotter:
                                       self.data.info['config'])
 
     def create_slides(self,
-                      savefile, 
-                    pol_r, 
-                    tor_r, 
-                    surfs, 
-                    tor_ind, 
-                    title, 
-                    corr_v = 1, 
-                    color_line = 'k', 
-                    color_ref = 'r'):
+                      pol_r, 
+                      tor_r, 
+                      corr_v = 1, 
+                      color_line = 'k', 
+                      color_ref = 'r'):
 
-        savefile = self.save_path + '/' + savefile
+        savefile = self.save_path + '/' + self.save_title + '.pdf'
+
+        x, y, _, _ = acc.return_view_xy(self.data)
+
+        ref_pol = acc.return_coord(self.data, 'Poloidal coordinate')
+        ref_tor = acc.return_coord(self.data, 'Toroidal coordinate')
+
+        pol_r = flh.process_selection(pol_r)
+        tor_r_pro = flh.process_selection(tor_r)
+
+        title = self.data.exp_id + ', ' + \
+                self.data.info['config'] + ', ' + \
+                self.data.info['view'].viewpoint.split('-')[1]
 
         with PdfPages(savefile) as pdf:
-            for i in range(self.data.shape[3]): 
-                fig, axes = plt.subplots(2,2, sharex=True, sharey=True)
-                fig.set_size_inches(10.08, 7.56)
-                axes[0,0].set_ylim(y[0], y[-1])
-                axes[0,0].set_xlim(x[0], x[-1])
+            for i in range(len(ref_tor)): 
                 mid_p = int(np.floor(self.data.shape[2]) / 2)
-                acc.plot_corr(fig, 
-                        axes.ravel()[0], 
-                        self.data.data[:,:,mid_p-4].T[::-1,:], 
-                        -corr_v, 
-                        corr_v, 
-                        'XCorr', 
-                        lines[0, i, tor_ind], 
-                        lines[1, i, tor_ind], 
-                        lines[:, pol_r, :], 
-                        tor_r, 
-                        color_line,  
-                        color_ref, 
-                        title + f', CCF, t = -44' +  r'$\mu$s', 
-                        surfs, 
-                        x, 
-                        y)
-                plot_ref_line(axes.ravel()[0], lines, i, tor_rp, color_ref)
-                for j in range(3):
-                    plot_corr(fig, 
-                            axes.ravel()[j + 1], 
-                            data_ccf.data[:,:,mid_p+2*j].T[::-1,:], 
-                            -corr_v, 
-                            corr_v, 
-                            'XCorr', 
-                            lines[0, i, tor_ind], 
-                            lines[1, i, tor_ind], 
-                            lines[:, pol_r, :], 
-                            tor_r, 
-                            color_line,  
-                            color_ref, 
-                            title + f', CCF, t = {j*22}' +  r'$\mu$s', 
-                            surfs, 
-                            x, 
-                            y)
-                    plot_ref_line(axes.ravel()[j + 1], lines, i, tor_rp, color_ref)
+                fig, axes = self.make_slide_layout(x, y)
+                t_lags = [-4, -2, 0, 4]
+                for j in range(4):
+                    t_j = t_lags[j]
+                    acc.plot_corr(fig, 
+                                  axes.ravel()[j], 
+                                  self.data.data[:,:,mid_p + t_j], 
+                                  -corr_v, 
+                                  corr_v, 
+                                  'XCorr', 
+                                  self.lines[0, ref_pol[i], ref_tor[i]], 
+                                  self.lines[1, ref_pol[i], ref_tor[i]], 
+                                  self.lines[:, pol_r, :], 
+                                  tor_r, 
+                                  color_line,  
+                                  color_ref, 
+                                  title + f', CCF, t = {t_j*11}' +  r'$\mu$s', 
+                                  self.surfs, 
+                                  x, 
+                                  y)
+                    acc.plot_ref_line(axes.ravel()[j], self.lines, ref_pol[i], tor_r_pro, color_ref)
                 pdf.savefig()
+                fig, axes = self.make_slide_layout(x, y)
+                t_lags = [-4, 0, 2, 4]
+                for j in range(4):
+                    t_j = t_lags[j]
+                    acc.plot_corr(fig, 
+                                  axes.ravel()[j], 
+                                  self.data.data[:,:,mid_p + t_j], 
+                                  -corr_v, 
+                                  corr_v, 
+                                  'XCorr', 
+                                  self.lines[0, ref_pol[i], ref_tor[i]], 
+                                  self.lines[1, ref_pol[i], ref_tor[i]], 
+                                  self.lines[:, pol_r, :], 
+                                  tor_r, 
+                                  color_line,  
+                                  color_ref, 
+                                  title + f', CCF, t = {t_j*11}' +  r'$\mu$s', 
+                                  self.surfs, 
+                                  x, 
+                                  y)
+                    acc.plot_ref_line(axes.ravel()[j], self.lines, ref_pol[i], tor_r_pro, color_ref)
+                pdf.savefig()
+
+    def make_slide_layout(self, x, y):
+        fig, axes = plt.subplots(2,2, sharex=True, sharey=True)
+        fig.set_size_inches(10.08, 7.56)
+        axes[0,0].set_ylim(y[0], y[-1])
+        axes[0,0].set_xlim(x[0], x[-1])
+        return fig, axes
