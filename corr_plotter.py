1c9c8a31d41a8c029107dd2ea3eafcc22261c087
lordofbejgli <kanyur@gmail.com>
2022-02-24 14:40:25 +0100

    Add make_slides_t_lag.

--- corr_plotter.py
+++ corr_plotter.py
@@ -21,7 +21,7 @@ import flap
 
 class CorrPlotter:
 
-    def __init__(self, corr_file, plot_surfs=None, ref_tor=3625, save_path=None) -> None:
+    def __init__(self, corr_file, plot_surfs=None, ref_tor=3425, save_path=None) -> None:
         f = io.open(corr_file, 'rb')
         
         self.data = pickle.load(f)
@@ -131,3 +131,74 @@ class CorrPlotter:
         axes[0,0].set_ylim(y[0], y[-1])
         axes[0,0].set_xlim(x[0], x[-1])
         return fig, axes
+
+    def make_slides_t_lag(self,
+                      pol_r, 
+                      tor_r, 
+                      corr_v = 1, 
+                      color_line = 'k', 
+                      color_ref = 'r'):
+
+        savefile = self.save_path + '/ ' + self.save_title + '_t_lag.pdf'
+
+        x, y, _, _ = acc.return_view_xy(self.data)
+
+        ref_pol = acc.return_coord(self.data, 'Poloidal coordinate')
+        ref_tor = acc.return_coord(self.data, 'Toroidal coordinate')
+
+        pol_r = flh.process_selection(pol_r)
+        tor_r_pro = flh.process_selection(tor_r)
+        
+        t = acc.return_coord(self.data, 'Time lag')
+        dt = (t[1] - t[0]) * 10**6
+
+        title = self.data.exp_id + ', ' + \
+                self.data.info['config'] + ', ' + \
+                self.data.info['view'].viewpoint.split('-')[1]
+
+        with PdfPages(savefile) as pdf:
+            for i in range(len(ref_tor)): 
+                max_p = t[np.argmax(self.data[:,:,:,i], axis=2)]
+                fig, axes = self.make_slide_layout(x, y)
+                fig.suptitle(f'Field Line: {ref_pol[i]}')
+                t_lags = [3, 7]
+                for j in range(2):
+                    t_j = t_lags[j]
+                    acc.plot_corr(fig, 
+                                  axes.ravel()[j], 
+                                  max_p, 
+                                  -dt * t_j, 
+                                  dt * t_j, 
+                                  r'Time lag ($\mu$s)', 
+                                  self.lines[0, ref_pol[i], ref_tor[i]], 
+                                  self.lines[1, ref_pol[i], ref_tor[i]], 
+                                  self.lines[:, pol_r, :], 
+                                  tor_r, 
+                                  color_line,  
+                                  color_ref, 
+                                  title + ', CCF Max Offset', 
+                                  self.surfs, 
+                                  x, 
+                                  y)
+                    acc.plot_ref_line(axes.ravel()[j], self.lines, ref_pol[i], tor_r_pro, color_ref)
+                
+                max_v = np.amax(self.data[:,:,:,i], axis=2)
+                acc.plot_corr(fig, 
+                              axes.ravel()[3], 
+                              max_v, 
+                              -corr_v, 
+                              corr_v, 
+                              'XCorr Max', 
+                              self.lines[0, ref_pol[i], ref_tor[i]], 
+                              self.lines[1, ref_pol[i], ref_tor[i]], 
+                              self.lines[:, pol_r, :], 
+                              tor_r, 
+                              color_line,  
+                              color_ref, 
+                              title + ', Max CCF', 
+                              self.surfs, 
+                              x, 
+                              y)
+                acc.plot_ref_line(axes.ravel()[3], self.lines, ref_pol[i], tor_r_pro, color_ref)
+                pdf.savefig()
+                plt.close()
